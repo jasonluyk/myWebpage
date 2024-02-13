90d86cde24a4e4f189d482b241134bf8f263851f
Jason Luyk <jasonluyk@gmail.com>
2024-02-13 12:08:32 -0500

    added unit converter

--- myWebsite/javascriptCode.js
+++ myWebsite/javascriptCode.js
@@ -8,3 +8,17 @@ function displayOutput() {
 }
 
 button.addEventListener("click", displayOutput);
+
+
+function convert(){
+    const inputValue = document.getElementById("userInput").value;
+    const unit = document.getElementById("unit").value;
+    const milesToKm = unit ==="milesToKm";
+    let result = 0;
+    if (milesToKm == true){
+      result = inputValue * 1.60934;
+      }else {
+        result = inputValue / 1.60934;
+      }
+    document.getElementById("resultElement").innerHTML = result;
+    }
