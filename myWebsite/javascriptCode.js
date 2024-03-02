c8dd31ad85c712f3874ba1a8cdd8a131224aa58b
jasonluyk <jasonluyk1@gmail.com>
2024-03-01 20:12:14 -0500

    Full update

--- myWebsite/javascriptCode.js
+++ myWebsite/javascriptCode.js
@@ -1,14 +1,8 @@
-var input = document.getElementById("input");
-var button = document.getElementById("button");
-var output = document.getElementById("output");
-
-function displayOutput() {
-    const text = input.value;
-    output.innerHTML = text;
-}
-
-button.addEventListener("click", displayOutput);
-
+function showDiv(divId, divId2, divId3, element) {
+    document.getElementById(divId).style.display = element.value == 1 ? 'block' : 'none';
+    document.getElementById(divId2).style.display = element.value == 2 ? 'block' : 'none';
+    document.getElementById(divId3).style.display = element.value == 3 ? 'block' : 'none';
+  }   
 
 function convert(){
     const inputValue = document.getElementById("userInput").value;
@@ -31,7 +25,10 @@ function convert(){
     let number = random();
     let guesses = 1;
     
+
+
     function checkGuess() {
+      
       const inputElement = document.getElementById("guess");
       const feedbackElement = document.getElementById("feedback");
       let guess = inputElement.value;
@@ -41,6 +38,7 @@ function convert(){
         feedbackElement.style.color = "green";
         number = Math.floor(Math.random() * 100) + 1;
         guesses = 1;
+        document.getElementById("guess").value = "";
       } else if (guess < number) {
         feedbackElement.innerHTML = "Too low! Try again.";
         feedbackElement.style.color = "red";
@@ -50,5 +48,69 @@ function convert(){
         feedbackElement.style.color = "red";
         guesses += 1;
       }
-      console.log(number);
+      
     }
+
+    function randomClickX() {
+      let randomX1 = Math.ceil(Math.random() * 10 );
+      let randomX2 = Math.ceil(Math.random() * 10 );
+      let randomX3 = Math.ceil(Math.random() * 10 );
+  
+      document.getElementById("randomX1").innerHTML = randomX1;
+      document.getElementById("randomX2").innerHTML = randomX2;
+      document.getElementById("randomX3").innerHTML = randomX3;
+  
+      let output = "Play again";
+      if (randomX1 === randomX2) {
+      if(randomX2 === randomX3) {
+          output = "Jackpot";
+          }
+      }
+  
+  
+      let chance = output;
+      function clickChance() {
+          document.getElementById("chance").innerHTML = chance;
+      }
+      document.getElementById('spanId').innerHTML = chance;
+  };
+
+  function createEnclosingDiv() {
+    let enclosingDiv = document.createElement("div");
+    enclosingDiv.classList.add("comment");
+    return enclosingDiv;
+    
+  }
+  
+  function createAvatar(initials) {
+    let newAvatar = document.createElement("span");
+    newAvatar.innerText = initials;
+    newAvatar.classList.add("avatar");
+    return newAvatar;
+  }
+  
+  function createText(text) {
+    let newText = document.createElement("span");
+    newText.classList.add("text");
+    newText.innerText = text;
+    return newText;
+  }
+  function resetFields(){
+    document.getElementById("initials").value = "";
+    document.getElementById("commentText").value = "";
+  }
+  
+  
+  function addComment() {
+   let commentsDiv = document.getElementById("commentSection");
+    let initialsInput = document.getElementById("initials").value;
+    let textInput = document.getElementById("commentText").value;
+  
+    let enclosingDiv = createEnclosingDiv();
+    let avatar = createAvatar(initialsInput);
+    let text = createText(textInput);
+    enclosingDiv.appendChild(avatar);
+    enclosingDiv.appendChild(text);
+    commentsDiv.appendChild(enclosingDiv);
+    resetFields();
+  }
